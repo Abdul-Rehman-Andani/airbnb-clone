29491c94cbc3367f51b75807861ee5c9614f7110
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2024-09-29 22:34:46 +0500

    next-auth

---
+++ models/user.ts
@@ -0,0 +1,9 @@
+import { Schema, models, model } from "mongoose";
+
+const userSchema = new Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, trim: true, unique: true },
+  password: { type: String, required: true, trim: true },
+}, {timestamps: true});
+
+export const User = models?.users || model("User", userSchema);
